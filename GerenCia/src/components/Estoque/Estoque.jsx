af967ea0fbd82cecd834cb231a2105f673f122b4
Ian Sampaio <ianosampaio@gmail.com>
2023-07-05 16:53:37 -0300

    add estoque interface

--- GerenCia/src/components/Estoque/Estoque.jsx
+++ GerenCia/src/components/Estoque/Estoque.jsx
@@ -1,13 +1,136 @@
-import styled from "styled-components";
 import { Link } from "react-router-dom";
 import Form from "./Form";
+import { useState } from "react";
+import styled from "styled-components";
 
-export default function Estoque() {
+const Button = styled.button`
+  background-color: #222;
+  color: #fff;
+  padding: 0.7em 1.2em;
+  text-decoration: none;
+  transition: 0.5s;
+  cursor: pointer;
+  border: none;
+  text-transform: uppercase;
+  max-width: 100px;
+  margin: 1em;
+  &:hover {
+    color: #f26b0c;
+  }
+`;
+
+const Content = styled.div`
+  display: ${({ isVisible }) => (isVisible ? "block" : "none")};
+  background-color: #f9f9f9;
+  padding: 0.5em;
+  margin-left: 24.5em;
+  width: 400px;
+`;
+
+const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const Table = styled.table`
+  border-collapse: collapse;
+  caption {
+    font-size: 1.5em;
+    font-weight: bolder;
+    padding: 1em;
+    text-transform: uppercase;
+  }
+
+  th{
+    background-color:white;
+  }
+
+  th,
+  td {
+    border: 1px solid black;
+    padding: .2em;
+  }
+
+  tr:nth-child(even) {
+    background-color: white;
+  }
+`;
+
+const ToggleButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleClick = () => {
+    setIsVisible(!isVisible);
+  };
+
   return (
-    <>
-      <Link to="/estoque/adicionar">
-        adicionar
-      </Link>
-    </>
+    <Div>
+      <Button onClick={handleClick}>{isVisible ? "fechar" : "Adiconar"}</Button>
+      {isVisible && (
+        <Content isVisible={isVisible}>
+          <Form />
+        </Content>
+      )}
+
+      <Table>
+        <caption>Estoque</caption>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Produto</th>
+            <th>Qnt</th>
+            <th>Preço Total</th>
+            <th>Preço Und</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>01</td>
+            <td>cebola</td>
+            <td>280</td>
+            <td>320</td>
+            <td>1</td>
+          </tr>
+          <tr>
+            <td>02</td>
+            <td>pão</td>
+            <td>500</td>
+            <td>500</td>
+            <td>1</td>
+          </tr>
+          <tr>
+            <td>03</td>
+            <td>sal</td>
+            <td>100</td>
+            <td>50</td>
+            <td>0.50</td>
+          </tr>
+          <tr>
+            <td>01</td>
+            <td>cebola</td>
+            <td>280</td>
+            <td>320</td>
+            <td>1</td>
+          </tr>
+          <tr>
+            <td>01</td>
+            <td>cebola</td>
+            <td>280</td>
+            <td>320</td>
+            <td>1</td>
+          </tr>
+          <tr>
+            <td>01</td>
+            <td>cebola</td>
+            <td>280</td>
+            <td>320</td>
+            <td>1</td>
+          </tr>
+        </tbody>
+      </Table>
+    </Div>
   );
-}
+};
+
+export default ToggleButton;
--- GerenCia/src/components/Estoque/Form.jsx
+++ GerenCia/src/components/Estoque/Form.jsx
@@ -1,15 +1,40 @@
-import styled from 'styled-components';
-import Input from '../form/Input';
-import Select from '../form/Select';
-import Button from '../form/Button';
-import { Link } from 'react-router-dom';
+import styled from "styled-components";
+import Input from "../form/Input";
+import Select from "../form/Select";
+import Button from "../form/Button";
 
-export default function Form(){
-    return(
-        <>
-            <Input/>
 
-            <Link to='/estoque'>voltar</Link>
-        </>
-    )
+
+
+
+export default function Form() {
+  
+    const submit = (e) => {
+        e.preventDefault();
+        handleSubmit(project);
+      };
+  
+    return (
+    <form onSubmit={submit} >
+      <Input
+        text="Produto:"
+        type="text"
+        placeholder="Insira o tipo de produto"
+      />
+
+      <Input
+        text="Quantidade:"
+        type="number"
+        placeholder="Insira a quantidade"
+      />
+
+      <Input
+        text="Valor total:"
+        type="number"
+        placeholder="Insira o valor total"
+      />
+
+      <Button onSubmit={submit} text='enviar'/>
+    </form>
+  );
 }
